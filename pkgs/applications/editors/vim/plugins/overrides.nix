60ed22d8567199c12da40fd10a8689d45d8b65a4
Alexei Robyn <shados@shados.net>
2025-03-19 14:39:25 +1100

    Cherry-pick PR 390702

--- pkgs/applications/editors/vim/plugins/overrides.nix
+++ pkgs/applications/editors/vim/plugins/overrides.nix
@@ -1263,6 +1263,7 @@ in
     dependencies = [ self.plenary-nvim ];
   };
 
+  # NOTE: this overrides a luaPackages-based plugin
   gitsigns-nvim = super.gitsigns-nvim.overrideAttrs {
     dependencies = [ self.plenary-nvim ];
   };
@@ -1403,6 +1404,7 @@ in
     doInstallCheck = true;
   };
 
+  # NOTE: this overrides a luaPackages-based plugin
   image-nvim = super.image-nvim.overrideAttrs {
     dependencies = with self; [
       nvim-treesitter
@@ -1665,6 +1667,7 @@ in
     dependencies = [ self.lualine-nvim ];
   };
 
+  # NOTE: this overrides a luaPackages-based plugin
   luasnip = super.luasnip.overrideAttrs {
     dependencies = [ luaPackages.jsregexp ];
   };
@@ -1931,6 +1934,7 @@ in
     ];
   };
 
+  # NOTE: this overrides a luaPackages-based plugin
   neotest = super.neotest.overrideAttrs {
     dependencies = with self; [
       nvim-nio
@@ -2807,6 +2811,7 @@ in
   };
 
   # needs  "http" and "json" treesitter grammars too
+  # NOTE: this overrides a luaPackages-based plugin
   rest-nvim = super.rest-nvim.overrideAttrs {
     dependencies = with self; [
       plenary-nvim
@@ -3194,6 +3199,7 @@ in
     dependencies = [ self.plenary-nvim ];
   };
 
+  # NOTE: this overrides a luaPackages-based plugin
   telescope-nvim = super.telescope-nvim.overrideAttrs {
     dependencies = [ self.plenary-nvim ];
   };
@@ -3961,101 +3967,6 @@ in
     '';
 
   };
-}
-// (
-  let
-    nodePackageNames = [
-      "coc-cmake"
-      "coc-docker"
-      "coc-emmet"
-      "coc-eslint"
-      "coc-explorer"
-      "coc-flutter"
-      "coc-git"
-      "coc-go"
-      "coc-haxe"
-      "coc-highlight"
-      "coc-html"
-      "coc-java"
-      "coc-jest"
-      "coc-json"
-      "coc-lists"
-      "coc-ltex"
-      "coc-markdownlint"
-      "coc-pairs"
-      "coc-prettier"
-      "coc-r-lsp"
-      "coc-rls"
-      "coc-rust-analyzer"
-      "coc-sh"
-      "coc-smartf"
-      "coc-snippets"
-      "coc-solargraph"
-      "coc-spell-checker"
-      "coc-sqlfluff"
-      "coc-stylelint"
-      "coc-sumneko-lua"
-      "coc-tabnine"
-      "coc-texlab"
-      "coc-tsserver"
-      "coc-ultisnips"
-      "coc-vetur"
-      "coc-vimlsp"
-      "coc-vimtex"
-      "coc-wxml"
-      "coc-yaml"
-      "coc-yank"
-    ];
-    nodePackage2VimPackage =
-      name:
-      buildVimPlugin {
-        pname = name;
-        inherit (nodePackages.${name}) version meta;
-        src = "${nodePackages.${name}}/lib/node_modules/${name}";
-      };
-  in
-  lib.genAttrs nodePackageNames nodePackage2VimPackage
-)
-// (
-  let
-    luarocksPackageNames = [
-      "fidget-nvim"
-      "gitsigns-nvim"
-      "image-nvim"
-      "lsp-progress-nvim"
-      "lualine-nvim"
-      "luasnip"
-      "lush-nvim"
-      "lz-n"
-      "lze"
-      "lzextras"
-      "lzn-auto-require"
-      "middleclass"
-      "mini-test"
-      "neorg"
-      "neotest"
-      "nui-nvim"
-      "nvim-cmp"
-      "nvim-nio"
-      "nvim-web-devicons"
-      "oil-nvim"
-      "orgmode"
-      "papis-nvim"
-      "rest-nvim"
-      "rocks-config-nvim"
-      "rtp-nvim"
-      "telescope-manix"
-      "telescope-nvim"
-    ];
-    toVimPackage =
-      name:
-      neovimUtils.buildNeovimPlugin {
-        luaAttr = luaPackages.${name};
-      };
-  in
-  lib.genAttrs luarocksPackageNames toVimPackage
-)
-// {
 
   rocks-nvim =
     (neovimUtils.buildNeovimPlugin {
